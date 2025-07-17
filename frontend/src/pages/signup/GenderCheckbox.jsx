b507c0cb714148143454f0253ad609c30dcc3ebb
Jeschwazermann <slaetunechi@gmail.com>
2025-07-17 12:27:05 +0100

    functionalities update for signup, logout

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,19 +1,37 @@
 import React from "react";
 
-const GenderCheckbox = () => {
+const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
   return (
     <div className="flex">
       <div className="form-control">
-        <label className={`label p-2 gap-2 cursor-pointer`}>
+        <label
+          className={`label p-2 gap-2 cursor-pointer ${
+            selectedGender === "male" ? "selected" : ""
+          }`}
+        >
           <span className="label-text">Male</span>
-          <input type="checkbox" className="checkbox border-slate-800" />
+          <input
+            type="checkbox"
+            className="checkbox border-slate-800"
+            checked={selectedGender === "male"}
+            onChange={() => onCheckboxChange("male")}
+          />
         </label>
       </div>
 
       <div className="form-control">
-        <label className={`label p-2 gap-2 cursor-pointer`}>
+        <label
+          className={`label p-2 gap-2 cursor-pointer ${
+            selectedGender === "female" ? "selected" : ""
+          }`}
+        >
           <span className="label-text">female</span>
-          <input type="checkbox" className="checkbox border-slate-800" />
+          <input
+            type="checkbox"
+            className="checkbox border-slate-800"
+            checked={selectedGender === "female"}
+            onChange={() => onCheckboxChange("female")}
+          />
         </label>
       </div>
     </div>
